1b2f70f0adb7454686a0ff608314a48b501255f4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 13:29:48 +0200

    Changed createGunConfigurations to get gun data from database

--- src/main/java/me/noaz/testplugin/tasks/GameController.java
+++ src/main/java/me/noaz/testplugin/tasks/GameController.java
@@ -22,6 +22,9 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -60,82 +63,148 @@ public class GameController {
      * Starts the timer.
      * @param plugin This plugin
      */
-    public GameController(TestPlugin plugin) {
+    public GameController(TestPlugin plugin, Connection connection) {
         this.plugin = plugin;
-        createGunConfigurations();
+        createGunConfigurations(connection);
         loadMaps();
         runTasks();
     }
 
-    private void createGunConfigurations() {
+    private void createGunConfigurations(Connection connection) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    PreparedStatement getGunConfigurationsFromDatabase = connection.prepareStatement("SELECT * FROM test.gun_configuration");
+                    ResultSet result = getGunConfigurationsFromDatabase.executeQuery();
+
+                    while(result.next()) {
+                        String name = result.getString("gun_name");
+                        String gunMaterial = result.getString("gun_material");
+                        String weaponType = result.getString("weapon_type");
+                        String fireType = result.getString("fire_type");
+                        float accuracyNotScoped = result.getFloat("accuracy_not_scoped");
+                        float accuracyScoped = result.getFloat("accuracy_scoped");
+                        float bodyDamage = result.getFloat("body_damage");
+                        float headDamage = result.getFloat("head_damage");
+                        float bulletSpeed = result.getFloat("bullet_speed");
+                        int gunRange = result.getInt("gun_range");
+                        int reloadTimeInMs = result.getInt("reload_time_in_ms");
+                        int burstDelayInMs = result.getInt("burst_delay_in_ms");
+                        int bulletsPerBurst = result.getInt("bullets_per_burst");
+                        int bulletsPerClick = result.getInt("bullets_per_click");
+                        int startingBullets = result.getInt("starting_bullets");
+                        int clipSize = result.getInt("clip_size");
+                        int loadoutSlot = result.getInt("loadout_slot");
+                        int unlockLevel = result.getInt("unlock_level");
+                        int costToBuy = result.getInt("cost_to_buy");
+                        String fireBulletSound = result.getString("fire_bullet_sound");
+                        String fireWhileReloadingSound = result.getString("fire_while_reloading_sound");
+                        String fireWithoutAmmoSound = result.getString("fire_without_ammo_sound");
+
+                        gunConfigurations.put(name, new WeaponConfiguration(name, gunMaterial, weaponType,
+                                fireType, accuracyNotScoped, accuracyScoped, bodyDamage, headDamage,
+                                bulletSpeed, gunRange, reloadTimeInMs, burstDelayInMs, bulletsPerBurst,
+                                bulletsPerClick, startingBullets, clipSize, loadoutSlot, unlockLevel,
+                                costToBuy, fireBulletSound, fireWhileReloadingSound, fireWithoutAmmoSound));
+
+
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskAsynchronously(plugin);
+
+
         //Should grab the information for all guns from database later on
 
         // http://puttyland.com/share/TTYWy1vN.txt
-        gunConfigurations.put("Skullcrusher", new WeaponConfiguration("Skullcrusher", Material.GOLD_INGOT,
+        /*gunConfigurations.put("Skullcrusher", new WeaponConfiguration("Skullcrusher", "GOLD_INGOT",
                 "Automatic", "burst",
                 2.0, 100.0, 7.2,7.5,4,
                 76,4000, 400,3, 1,72,24,
                 new Sound[] {Sound.ENTITY_SKELETON_HURT, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, Sound.ENTITY_GHAST_SHOOT},
                 6, 13));
 
-        gunConfigurations.put("Python", new WeaponConfiguration("Python", Material.GOLDEN_SHOVEL,
+        gunConfigurations.put("Python", new WeaponConfiguration("Python", "GOLDEN_SHOVEL",
                 "Secondary", "single",
                 2.0, 100.0, 10.0, 15.0, 4,
                 48, 3500, 300, 1, 1,50, 9,
                 new Sound[] {Sound.ENTITY_GHAST_SHOOT, Sound.ENTITY_ZOMBIE_ATTACK_IRON_DOOR}, 1, 18));
 
+        /*
+        SELECT * FROM test.player WHERE player_uuid=uuid;
+SELECT * FROM test.owned_gun_list WHERE id=(player.owned_guns);
+         */
+        /* WORKS:
+
+        SELECT * FROM test.gun_configuration
+INNER JOIN test.player_own_gun ON test.gun_configuration.gun_id=test.player_own_gun.gun_id
+WHERE player_own_gun.player_id=5
+
+// INSERT INTO test.player_own_gun (player_id, gun_id) VALUES (5,1); Inserts gun with player id 5 and gun nr 1
+
+         */
+
+        // INNER JOIN test.owned_gun_list ON test.player.owned_guns=test.owned_gun_list.id
+        /*INSERT INTO test.gun_configuration (gun_name, gun_material, weapon_type, fire_type, accuracy_not_scoped, accuracy_scoped, body_damage,
+                head_damage, bullet_speed, gun_range, reload_time_in_ms, burst_delay_in_ms, bullets_per_burst, bullets_per_click, starting_bullets,
+                clip_size, loadout_slot, unlock_level, cost_to_buy, fire_bullet_sound, fire_while_reloading_sound, fire_without_ammo_sound) VALUES
+                ('Skullcrusher', 'GOLD_INGOT', 'Automatic', 'burst', 2.0, 100, 7.2, 7.5, 4, 76, 4000, 400, 3, 1, 72, 24, 13, 6, 300, 'ENTITY_SKELETON_HURT',
+                        'ENTITY_ZOMBIE_BREAK_WOODEN_DOOR', 'ENTITY_GHAST_SHOOT');*/
         //One sound is quiet
-        gunConfigurations.put("Dragunov", new WeaponConfiguration("Dragunov", Material.STONE_AXE,
+        /*gunConfigurations.put("Dragunov", new WeaponConfiguration("Dragunov", "STONE_AXE",
                 "Sniper", "single",
                 2.0, 100.0, 11.4, 20.0, 4,
                 400, 4250, 500, 1, 1,32, 7,
                 new Sound[] {Sound.ENTITY_IRON_GOLEM_ATTACK, Sound.ENTITY_WITHER_SHOOT}, 1, 19));
 
-        gunConfigurations.put("L120 Isolator", new WeaponConfiguration("L120 Isolator", Material.BOWL,
+        gunConfigurations.put("L120 Isolator", new WeaponConfiguration("L120 Isolator", "BOWL",
                 "Sniper", "single",
                 2.0, 100.0, 21.4, 25.0, 4,
                 400, 5000, 900, 1, 1,60, 4,
                 new Sound[] {Sound.ENTITY_IRON_GOLEM_ATTACK, Sound.ENTITY_WITHER_SHOOT}, 2, 20));
 
-        gunConfigurations.put("AA12", new WeaponConfiguration("AA12", Material.IRON_PICKAXE,
+        gunConfigurations.put("AA12", new WeaponConfiguration("AA12", "IRON_PICKAXE",
                 "Shotgun", "buck", 1.0, 3.0, 7,
                 9, 4, 18, 1500, 450, 1,
                 6, 32, 20, new Sound[] {Sound.ENTITY_GHAST_SHOOT, Sound.ENTITY_GENERIC_EXPLODE},
                 1, 28));
 
-        gunConfigurations.put("Typhoid", new WeaponConfiguration("Typhoid", Material.BRICK, "Shotgun",
+        gunConfigurations.put("Typhoid", new WeaponConfiguration("Typhoid", "BRICK", "Shotgun",
                 "buck", 1.0,3.0, 3.6, 4.0, 4,
                 13, 3500, 500, 3, 4, 32, 12,
                 new Sound[] {Sound.ENTITY_GHAST_SHOOT, Sound.ENTITY_GENERIC_EXPLODE}, 3, 29));
                 //10 or 11 or 24 tick delay?
 
-        gunConfigurations.put("AK-47", new WeaponConfiguration("AK-47", Material.IRON_HOE,
+        gunConfigurations.put("AK-47", new WeaponConfiguration("AK-47", "IRON_HOE",
                 "Automatic", "automatic", 3, 100.0,
                 4.4, 6, 4, 76, 1500, 150,
                 1, 1,100, 30,
                 new Sound[] {Sound.ENTITY_SKELETON_HURT, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, Sound.ENTITY_GHAST_SHOOT},
                 9, 10));
 
-        gunConfigurations.put("Lemantation", new WeaponConfiguration("Lemantation", Material.IRON_INGOT,
+        gunConfigurations.put("Lemantation", new WeaponConfiguration("Lemantation", "IRON_INGOT",
                 "Automatic", "automatic", 3, 100.0,
                 3.6, 4.5, 4, 76, 4000, 100,
                 1, 1,100, 50,
                 new Sound[] {Sound.ENTITY_SKELETON_HURT, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, Sound.ENTITY_GHAST_SHOOT},
                 7, 12));
 
-        gunConfigurations.put("Famas", new WeaponConfiguration("Famas", Material.DIAMOND_HOE,
+        gunConfigurations.put("Famas", new WeaponConfiguration("Famas", "DIAMOND_HOE",
                 "Automatic", "burst", 3,100.0,
                 5.8, 6.3, 4, 48, 3750, 300,
                 3, 1, 64, 30,
                 new Sound[] {Sound.BLOCK_NOTE_BLOCK_SNARE, Sound.ENTITY_ITEM_BREAK, Sound.ENTITY_GHAST_SHOOT},
                 5, 11));
 
-        gunConfigurations.put("Minigun", new WeaponConfiguration("Minigun", Material.DIAMOND,
+        gunConfigurations.put("Minigun", new WeaponConfiguration("Minigun", "DIAMOND",
                 "Automatic", "buck", 3, 10,
                 3.6, 4.5, 4, 128, 100, 50,
                 1, 2, 64, 64,
                 new Sound[] {Sound.ENTITY_SKELETON_HURT, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, Sound.ENTITY_GHAST_SHOOT},
-                10, 35));
+                10, 35));*/
     }
 
     private void loadMaps() {

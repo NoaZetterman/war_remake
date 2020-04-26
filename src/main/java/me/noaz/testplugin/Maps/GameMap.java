52748738fb34b54cf8aab0e88c4f9755b9d1c87e
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 00:14:57 +0200

    Remove debug println

--- src/main/java/me/noaz/testplugin/Maps/GameMap.java
+++ src/main/java/me/noaz/testplugin/Maps/GameMap.java
@@ -42,7 +42,6 @@ public class GameMap {
         if(world == null) {
             //Typ?
             world = Bukkit.getServer().createWorld(new WorldCreator(name));
-            System.out.println(world.getDifficulty());
             world.setDifficulty(Difficulty.PEACEFUL);
             System.out.println("Map loaded: " + name);
         }
@@ -111,77 +110,4 @@ public class GameMap {
     public World getWorld() {
         return world;
     }
-
-    /*private void reloadMap(String mapName) {
-
-
-        plugin.getServer().unloadWorld(plugin.getServer().getWorld(mapName), false);
-
-        WorldCreator creator = new WorldCreator(mapName);
-        World gameWorld = plugin.getServer().createWorld(creator);
-        if(gameWorld != null) {
-            gameWorld.setDifficulty(Difficulty.PEACEFUL);
-            gameWorld.setAmbientSpawnLimit(0);
-            gameWorld.setAnimalSpawnLimit(0);
-            gameWorld.setMonsterSpawnLimit(0);
-            gameWorld.setGameRule(GameRule.DO_MOB_SPAWNING, false);
-            gameWorld.setWaterAnimalSpawnLimit(0);
-            gameWorld.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
-            gameWorld.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
-            gameWorld.setTime(6000);
-            gameWorld.setGameRule(GameRule.NATURAL_REGENERATION, true);
-
-            gameWorld.setAutoSave(false);
-
-            Sign mapSize = (Sign) gameWorld.getBlockAt(gameWorld.getSpawnLocation()).getState();
-            gameWorld.getBlockAt(gameWorld.getSpawnLocation()).setType(Material.AIR);
-
-            String mostNegativeCornerLocation = mapSize.getLine(0);
-            String[] lowestCoordinate = mostNegativeCornerLocation.split(",");
-            int lowestX = Integer.parseInt(lowestCoordinate[0]);
-            int lowestY = Integer.parseInt(lowestCoordinate[1]);
-            int lowestZ = Integer.parseInt(lowestCoordinate[2]);
-
-            String mostPositiveCornerLocation = mapSize.getLine(1);
-            String[] highestCoordinate = mostPositiveCornerLocation.split(",");
-            int highestX = Integer.parseInt(highestCoordinate[0]);
-            int highestY = Integer.parseInt(highestCoordinate[1]);
-            int highestZ = Integer.parseInt(highestCoordinate[2]);
-
-            for (int x = lowestX; x < highestX; x++) {
-                for (int z = lowestZ; z < highestZ; z++) {
-                    for (int y = lowestY; y < highestY; y++) {
-                        if (gameWorld.getBlockAt(x, y, z).getState() instanceof Sign) {
-                            Location signLocation = new Location(gameWorld, x, y, z);
-                            Sign sign = (Sign) gameWorld.getBlockAt(x, y, z).getState();
-                            String eventualSpawnOrConfig = sign.getLine(0);
-                            boolean isConfigSign = true;
-
-                            switch (eventualSpawnOrConfig) {
-                                case "bluespawn":
-                                case "redspawn":
-                                case "ffaspawn":
-                                case "redflag":
-                                case "blueflag":
-                                    break;
-                                default:
-                                    isConfigSign = false;
-                                    break;
-                            }
-
-                            if (isConfigSign) {
-                                signLocation.getBlock().setType(Material.AIR);
-                            }
-
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    */
-
-    //More
-
 }
